7d102ccaf1e21f8725bb305efb41e492f4d83af5
Denis Tabakov <spbtabakoff@yandex.com>
2022-01-20 17:51:32 +0300

    add brain_games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+import prompt
+
 def main():
-    print("Welcome to the Brain Games!")
-    
+    print("Welcome to the Brain Games! - test")
+    name = prompt.string('May I have your name? ') 
+    print(name)
+
+
 if __name__=='__main__':
     main()
+    
